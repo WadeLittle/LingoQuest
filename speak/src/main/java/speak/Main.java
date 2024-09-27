ca4809b07263f8f6ab4f4df7943e00cf57774592
Cade Stocker <cadestocker@Cades-Air-2.lan>
2024-09-26 23:49:20 -0400

    Got Amazon Polly working.

---
+++ speak/src/main/java/speak/Main.java
@@ -0,0 +1,7 @@
+package speak;
+
+public class Main {
+    public static void main(String[] args) {
+        Narriator.playSound("The sprinkler goes like thisstststststststststststststststststststststststst and comes back like ttttttttttttttttttttttttttttttttttttttte please please please give me a good great this took so long");
+    }
+}
