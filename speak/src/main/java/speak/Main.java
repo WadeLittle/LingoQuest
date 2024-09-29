8a73c974db3609afe6f5707868c80c4b4439a17f
Cade Stocker <caderstocker@gmail.com>
2024-09-29 11:42:20 -0400

    completed docs folder

--- speak/src/main/java/speak/Main.java
+++ speak/src/main/java/speak/Main.java
@@ -2,6 +2,6 @@ package speak;
 
 public class Main {
     public static void main(String[] args) {
-        Narriator.playSound("The sprinkler goes like thisstststststststststststststststststststststststst and comes back like ttttttttttttttttttttttttttttttttttttttte please please please give me a good great this took so long");
+        Narriator.playSound("Me gusta bailar en la biblioteca......      The sprinkler goes like thisstststststststststststststststststststststststst and comes back like ttttttttttttttttttttttttttttttttttttttte please please please give me a good grade");
     }
 }
