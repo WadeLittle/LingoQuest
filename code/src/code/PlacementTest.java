2d9ec05aba40b7e89289da718596d2932fa0f9c4
WadeLittle <wdlittle9@gmail.com>
2024-10-03 16:01:59 -0400

    fixed #11

--- code/src/code/PlacementTest.java
+++ code/src/code/PlacementTest.java
@@ -1,5 +1,24 @@
 package code;
 
 public class PlacementTest {
-    
+    private ArrayList<Question> questions;
+    private int correctAnswers;
+    private int score;
+
+    public PlacementTest() {
+
+    }
+
+    public int getScore() {
+
+    }
+
+    public ArrayList<Question> getQuestions() {
+
+    }
+
+    public int getCorrectAnswers() {
+        
+    }
+
 }
