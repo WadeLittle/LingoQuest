fb4fd461349523c69835ccbe57ce3d920e699e32
WadeLittle <wdlittle9@gmail.com>
2024-10-03 16:43:20 -0400

    fixed #18

--- code/src/code/Video.java
+++ code/src/code/Video.java
@@ -1,5 +1,32 @@
 package code;
 
+import java.io.File;
+
 public class Video {
-    
+    private File video;
+    private String description;
+    private String name;
+
+    public Video() {
+
+    }
+
+    public Video(String name, String description) {
+
+    }
+
+    public File getVideo() {
+
+        return video;
+    }
+
+    public void setVideo(File video) {
+
+    }
+
+    public void play() {
+        
+    }
+
+
 }
