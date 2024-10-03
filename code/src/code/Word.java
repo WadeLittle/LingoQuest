8f75540e6e5700ae6572d06cdca6c273d9638cdd
WadeLittle <wdlittle9@gmail.com>
2024-10-03 16:14:55 -0400

    fixed #12

--- code/src/code/Word.java
+++ code/src/code/Word.java
@@ -1,5 +1,60 @@
 package code;
 
 public class Word {
-    
+   private int timesPresented;
+   private String word;
+   private int timesCorrect;
+   private double userUnderstanding;
+   
+   pubilc Word() {
+
+   }
+
+   public Word(String word) {
+
+   }
+
+   public void setWord(String word) {
+
+   }
+
+   public String toString() {
+
+    return " ";
+   }
+
+   public int getTimesPresented() {
+
+    return 0;
+   }
+
+   public int getTimesCorrect() {
+
+    return 0;
+   }
+
+   public double getUserUnderstanding() {
+
+    return 0;
+   }
+
+   public void wordPresented(boolean correct) {
+
+   }
+
+   private void updateTimesCorrect() {
+
+   }
+
+   private void updateTimesPresented() {
+
+   }
+
+   private void updateUserUnderstanding() {
+
+   }
+
+
+
+
 }
