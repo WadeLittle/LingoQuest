b48649fde3af2e175020ca5042c5366aaf36a5c3
Preston <prwillis2@gmail.com>
2024-10-05 16:57:29 -0400

    stubs

--- code/src/code/DataWriter.java
+++ code/src/code/DataWriter.java
@@ -1,5 +1,15 @@
 package code;
+import java.io.File;
 
-public class DataWriter {
-    
+class DataWriter {
+    private String filePath;
+    private ItemShop itemShop;
+    private Users users;
+    private LanguageManager languageManager;
+
+    public void writeData(File fileName) {
+    }
+
+    public void userLogout() {
+    }
 }
--- code/src/code/Dictionary.java
+++ code/src/code/Dictionary.java
@@ -1,5 +1,47 @@
 package code;
+import java.util.HashMap;
 
-public class Dictionary {
-    
+class Dictionary {
+    private HashMap<Word, Word> fromEnglish;
+    private HashMap<Word, Word> toEnglish;
+    private int numberOfWords;
+
+    public Dictionary() {
+        fromEnglish = new HashMap<>();
+        toEnglish = new HashMap<>();
+        numberOfWords = 0;
+    }
+
+    public void addWord(Word english, Word otherLanguage) {
+        fromEnglish.put(english, otherLanguage);
+        toEnglish.put(otherLanguage, english);
+        numberOfWords++;
+    }
+
+    public void removeWord(Word english, Word otherLanguage) {
+        fromEnglish.remove(english);
+        toEnglish.remove(otherLanguage);
+        numberOfWords--;
+    }
+
+    public void printDictionary(HashMap<Word, Word> map) {
+        for (Word key : map.keySet()) {
+            System.out.println(key + " -> " + map.get(key));
+        }
+    }
+
+    public boolean contains(Word word) {
+        return fromEnglish.containsKey(word) || toEnglish.containsKey(word);
+    }
+
+    public int getNumberOfWords() {
+        return numberOfWords;
+    }
+
+    public void loadDictionary() {
+    }
+
+    public void saveDictionary() {
+    }
 }
+
