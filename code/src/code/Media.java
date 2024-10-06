3480142e32e2a162cceb7a947e319ce278f409ea
Alok Patel <alokothro@gmail.com>
2024-10-06 14:42:11 -0400

    Media interface stub added

--- code/src/code/Media.java
+++ code/src/code/Media.java
@@ -1,6 +1,10 @@
 package code;
+
 public interface Media {
-
-
+    // Methods defined by the interface
+    void displayMedia();
     
+    String getName();
+    
+    void setName(String name);
 }
