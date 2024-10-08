3882d7986878a3c8dfba265c632c515a05327cf1
Alok Patel <alokothro@gmail.com>
2024-10-08 16:05:38 -0400

    started video class

--- code/src/code/Video.java
+++ code/src/code/Video.java
@@ -1,6 +1,14 @@
 package code;
 
 import java.io.File;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+import javafx.application.Platform;
 
 public class Video {
     private File video;
@@ -8,25 +16,26 @@ public class Video {
     private String name;
 
     public Video() {
-
+        this.name = "";
+        this.description = "";
+        this.video = null;
     }
 
     public Video(String name, String description) {
-
+        this.name = name;
+        this.description = description;
+        this.video = null;
     }
 
     public File getVideo() {
-
         return video;
     }
 
     public void setVideo(File video) {
-
+        this.video = video;
     }
 
     public void play() {
         
-    }
-
-
+}
 }
