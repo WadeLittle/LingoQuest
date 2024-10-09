b95664cc1db30cf4472f63642de271f0fcfc00c0
Cade Stocker <caderstocker@gmail.com>
2024-10-08 22:31:33 -0400

    closed #113

--- code/src/code/PlacementTest.java
+++ code/src/code/PlacementTest.java
@@ -19,9 +19,14 @@ public class PlacementTest {
         return this.score;
     }
 
+    /**
+     * @author Cade Stocker
+     * @return ArrayList of questions
+     * I left an issue on our scrum board asking how we will handle
+     * questions being generated for the placement test #223
+     */
     public ArrayList<Question> getQuestions() {
-
-        return questions;
+        return this.questions;
     }
 
     public int getCorrectAnswers() {
