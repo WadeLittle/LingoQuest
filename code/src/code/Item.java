8e64c62295624f53afa6ef8177789f51e7e43417
Cade Stocker <caderstocker@gmail.com>
2024-10-08 21:40:08 -0400

    closed #62

--- code/src/code/Item.java
+++ code/src/code/Item.java
@@ -1,4 +1,28 @@
 package code;
 public class Item {
-    
+  
+    private String name;
+    private String description;
+    private int price;
+    private boolean owned;
+
+    /**
+     * @author Cade Stocker
+     * default constructor doesn't need to do anything specific
+     */
+    public Item() {
+
+    }
+
+    /**
+     * @author Cade Stocker
+     * @param name of the item
+     * @param description of the item
+     * @param price of the item
+     */
+    public Item(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
 }
