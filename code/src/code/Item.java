16bbd1f1d9d220f5f4f6756835963c5eb40f437d
Preston <prwillis2@gmail.com>
2024-10-10 13:07:36 -0400

    Question

--- code/src/code/Item.java
+++ code/src/code/Item.java
@@ -1,4 +1,40 @@
 package code;
-public class Item {
-    
+class Item {
+    private String name;
+    private String description;
+    private int price;
+    private boolean owned;
+
+    // Constructor for Item class
+    public Item(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.owned = false;
+    }
+
+    // Gets the name of the item
+    public String getName() {
+        return name;
+    }
+
+    // Gets the description of the item
+    public String getDescription() {
+        return description;
+    }
+
+    // Gets the price of the item
+    public int getPrice() {
+        return price;
+    }
+
+    // Returns if the item is owned
+    public boolean getOwned() {
+        return owned;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{name='" + name + "', description='" + description + "', price=" + price + ", owned=" + owned + '}';
+    }
 }
