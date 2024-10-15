1ad5343daff5bc4b040114b64ac74bfad4ada34f
Cade Stocker <caderstocker@gmail.com>
2024-10-15 13:02:57 -0400

    fixed leaderboard file

--- correct_structure/src/main/java/lingo_quest/LeaderBoard.java
+++ correct_structure/src/main/java/lingo_quest/LeaderBoard.java
@@ -7,17 +7,17 @@ import java.util.Collections;
  * The Leaderboard class maintains a list of users and their rankings based on points.
  * It supports adding/removing users and sorting the leaderboard by the user's total points.
  */
-public class Leaderboard {
+public class LeaderBoard {
     
     // List to store users
     private ArrayList<User> users;
-    private static Leaderboard leaderboardInstance;
+    private static LeaderBoard leaderboardInstance;
 
     /**
      * Private constructor for the Leaderboard class.
      * Initializes the list of users.
      */
-    private Leaderboard() {
+    private LeaderBoard() {
         users = new ArrayList<>();
     }
 
@@ -26,9 +26,9 @@ public class Leaderboard {
      *
      * @return The singleton Leaderboard instance.
      */
-    public static Leaderboard getInstance() {
+    public static LeaderBoard getInstance() {
         if (leaderboardInstance == null) {
-            leaderboardInstance = new Leaderboard();
+            leaderboardInstance = new LeaderBoard();
         }
         return leaderboardInstance;
     }
@@ -64,7 +64,7 @@ public class Leaderboard {
     public void printLeaderboard() {
         System.out.println("Leaderboard:");
         for (int i = 0; i < users.size(); i++) {
-            System.out.println((i + 1) + ". " + users.get(i).getUsername() + " - " + users.get(i).getTotalPoints() + " points");
+            System.out.println((i + 1) + ". " + users.get(i).getUsername() + " - " + users.get(i).getCoinsEarned() + " points");
         }
     }
 
