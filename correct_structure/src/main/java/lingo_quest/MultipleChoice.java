b7ccfe68d3a4b9af0d8a558051b8cb8008ace7d6
WadeLittle <wdlittle9@gmail.com>
2024-10-21 11:53:02 -0400

    fixed #89

--- correct_structure/src/main/java/lingo_quest/MultipleChoice.java
+++ correct_structure/src/main/java/lingo_quest/MultipleChoice.java
@@ -1,14 +1,20 @@
 package lingo_quest;
 
+import java.io.File;
 import java.util.ArrayList;
 
-public class MultipleChoice {
+public class MultipleChoice extends Question {
     
+    /**
+     * @author Wade Little
+     * Created the constructor that calls on the Questions super constructor
+     */
     public MultipleChoice() {
-
+        super();
     }
 
-    public MultipleChoice(ArrayList<Word> answerChoices, Word answer) {
-        
+    
+    public MultipleChoice(int pointValue, int coinValue, String question, ArrayList<Word> answerChoices, Word correctAnswer, File media) {
+        super(pointValue, coinValue, question, answerChoices, correctAnswer, media);
     }
 }
