0d03bd9517e3788339ac657d9b40f7feeb3f22e9
WadeLittle <wdlittle9@gmail.com>
2024-10-21 12:23:32 -0400

    fixed #88

--- correct_structure/src/main/java/lingo_quest/FillInTheBlank.java
+++ correct_structure/src/main/java/lingo_quest/FillInTheBlank.java
@@ -1,8 +1,9 @@
 package lingo_quest;
 
+import java.io.File;
 import java.util.ArrayList;
 
-public class FillInTheBlank {
+public class FillInTheBlank extends Question{
     /**
      * @author Wade Little
      * Uses the super constructor to create a fill in the blank question
@@ -10,8 +11,18 @@ public class FillInTheBlank {
     public FillInTheBlank() {
         super();
     }
-
-    public FillInTheBlank(String question, ArrayList<Word> answerChoices, Word correctAnswer) {
-        
+    
+    /**
+     * @author Wade Little
+     * Created the paramaterized constructor based off the Super Class Question
+     * @param pointValue The point value of the question
+     * @param coinValue THe coin value of the question
+     * @param question The String of the question
+     * @param answerChoices The array list of answer Choices
+     * @param correctAnswer The correct answer to the question
+     * @param media The media associated to the question
+     */
+    public FillInTheBlank(int pointValue, int coinValue, String question, ArrayList<Word> answerChoices, Word correctAnswer, File media) {
+        super(pointValue, coinValue, question, answerChoices, correctAnswer, media);
     }
 }
