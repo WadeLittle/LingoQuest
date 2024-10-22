03932e15b6d7caa2981301353de62cff0a7e07e6
Cade Stocker <caderstocker@gmail.com>
2024-10-22 17:14:12 -0400

    more changes to dictionarymanager

--- correct_structure/src/main/java/lingo_quest/DictionaryManager.java
+++ correct_structure/src/main/java/lingo_quest/DictionaryManager.java
@@ -1,15 +1,24 @@
 package lingo_quest;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class DictionaryManager {
     private ArrayList<Dictionary> dictionaries;
     private static DictionaryManager managerObject;
 
+    /**
+     * @author cade
+     * private constructor for singleton
+     */
     private DictionaryManager() {
         this.dictionaries = new ArrayList<>();
     }
     
+    /**
+     * @author cade
+     * @return the instance of the class
+     */
     public static DictionaryManager getInstance() {
         if(managerObject == null)
             return new DictionaryManager();
@@ -17,7 +26,39 @@ public class DictionaryManager {
             return managerObject;
     }
 
+    /**
+     * @author cade
+     * @param d
+     * add a dictionary to the arraylist
+     */
     public void addDictionary(Dictionary d) {
         dictionaries.add(d);
+    }
+
+    /**
+     * @author cade
+     * @param id
+     */
+    public void removeDictionary(UUID id) {
+        for(Dictionary d : dictionaries) {
+            if(d.getID().equals(id)) {
+                dictionaries.remove(d);
+                return;
+            }
+        }
+    }
+
+    /**
+     * @author cade
+     * @param id
+     * @return
+     */
+    public Dictionary getDictionaryByID(UUID id) {
+        for(Dictionary d : dictionaries) {
+            if(d.getID().equals(id))
+                return d;
+        }
+        System.out.println("Dictionary doesn't exist.");
+        return null;
     }
 }
