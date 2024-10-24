aa9ab0a75abd4b355900045ecb4a0c746288bfb7
Preston <prwillis2@gmail.com>
2024-10-24 16:55:59 -0400

    questioncreate

--- correct_structure/src/main/java/lingo_quest/QuestionCreator.java
+++ correct_structure/src/main/java/lingo_quest/QuestionCreator.java
@@ -1,10 +1,18 @@
 package lingo_quest;
 
+
+
 public class QuestionCreator {
     
-    public Question makeQuestion(String questionType){
-
+    public Question makeQuestion(double wordValue){
+        if (wordPoint <= 0) {
+            return new MultipleChoice();
+        } else if (wordPoint == 100) {
+            return new FillInTheBlank();
+        } else if (wordPoint >= 200) {
+            return new Matching();
+        }
+        throw new IllegalArgumentException("Invalid wordPoint value");
     }
-    
 
 }
