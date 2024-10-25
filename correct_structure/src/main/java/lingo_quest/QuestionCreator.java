50b9e543a913eed942ecf5cc9d8dfe7fd49ea9fc
WadeLittle <wdlittle9@gmail.com>
2024-10-25 14:42:14 -0400

    improvements to question creator

--- correct_structure/src/main/java/lingo_quest/QuestionCreator.java
+++ correct_structure/src/main/java/lingo_quest/QuestionCreator.java
@@ -1,29 +1,29 @@
 package lingo_quest;
 
-import java.util.ArrayList;
-
 public class QuestionCreator {
 
-    
-    
-    // Constructor to initialize DictionaryManager
+    // Constructor
     public QuestionCreator() {
     }
     
     public Question createQuestion(Lesson lesson) {
         Word word = lesson.getRandomWord();
-        // Retrieve a word from the dictionary to use in the question
         int wordPoint = word.getPoints();
-        if (wordPoint <= 0) {
-            Word anotherWord = lesson.getRandomWord();
-            return new TrueOrFalse(lesson.getLanguageName(), word, anotherWord);
+        
+        if (wordPoint >= 300) {
+            return new Matching(lesson.getLanguageName(), lesson.getWords(word, 4));
+        } else if (wordPoint >= 200) {
+            return new FillInTheBlank(lesson.getLanguageName(), word);
         } else if (wordPoint == 100) {
             return new MultipleChoice(lesson.getLanguageName(), lesson.getWords(word, 4), word);
-        } else if (wordPoint >= 200) {
-            return new FillInTheBlank(lesson.getLanguageName(), lesson.getRandomWord());
-        } else if (wordPoint >= 300) {
-            return new Matching(lesson.getLanguageName(),lesson.getWords(word, 4));
+        } else if (wordPoint <= 0) {
+            Word anotherWord;
+            do {
+                anotherWord = lesson.getRandomWord();
+            } while (anotherWord.equals(word));
+            return new TrueOrFalse(lesson.getLanguageName(), word, anotherWord);
         }
-        throw new IllegalArgumentException("Invalid wordPoint value");
+        
+        throw new IllegalArgumentException("Unsupported wordPoint value: " + wordPoint);
     }
 }
